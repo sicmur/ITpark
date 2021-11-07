24c40b21f7201bfe02cd8991d59badf1826af1a8
sicmur <sicmur@yandex.ru>
2021-11-07 20:02:46 +0900

    add HomeWork №2: простоые арифметические операции и вызов метода / переименование старого файла

--- src/main/java/HomeWork1.java
+++ src/main/java/HomeWork1.java
@@ -1,4 +1,4 @@
-public class FirstProgram {
+public class HomeWork1 {
     public static void main(String [] args) {
         System.out.println("Привет Мир!");
         System.out.print("Это моя первая программа :)");
