192b2d47ec9229494f5190788eeacd86134c3b10
sicmur <93529021+sicmur@users.noreply.github.com>
2021-11-13 19:39:56 +0900

    Delete src/main/java directory
    reboot

--- src/main/java/HomeWork1.java
+++
@@ -1,6 +0,0 @@
-public class HomeWork1 {
-    public static void main(String [] args) {
-        System.out.println("Привет Мир!");
-        System.out.print("Это моя первая программа :)");
-    }
-}
